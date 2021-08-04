02fc77a5777e554b4f4328b2fe708fd880f7ea56
Valerie Robateau <valerie.robateau@ed-fi.org>
2021-08-04 12:34:11 -0500

    changed name

--- .teamcity/_self/buildTypes/BuildAndTestPullRequest.kt
+++ .teamcity/_self/buildTypes/BuildAndTestPullRequest.kt
@@ -5,7 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 object BuildAndTestPullRequest : BuildType({
     templates(_self.templates.CompileAndRunUnitTestsTemplate)
-    name = "Build and test pull request"
+    name = "Compile and test pull request"
 
     triggers {
         vcs {
